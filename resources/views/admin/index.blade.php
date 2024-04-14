334ab53d3274e0da320fcbde9948075b4ff138ac
Ashsifat1511 <sifatashrarul@gmail.com>
2024-04-14 08:17:52 +0600

    responsive

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrative</title>
-    <link rel="stylesheet" href="{{asset('css/admin/adminroute.css')}}">
+    <link rel="stylesheet" href="{{asset('css/admin.css')}}">
 </head>
 
 <body>
--- resources/views/rentals/index.blade.php
+++ resources/views/rentals/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rentals</title>
-    <link rel="stylesheet" href="css/rental/rental.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
